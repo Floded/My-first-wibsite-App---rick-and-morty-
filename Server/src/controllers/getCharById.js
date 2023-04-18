0db2aa29e9bf63288ea048bcb39987fcb5ade279
Alfredo Lillo <alphonselillo@gmail.com>
2023-04-18 01:13:44 -0300

    Integration login + search

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
+const URL = "https://rickandmortyapi.com/api/character/";
 
 function getCharById(req, res){
     const { id } = req.params;
+    console.log(id);
     try {
         axios.get(`${URL}${id}`).then(response => {
             const { id, name, image, gender, species } = response.data
@@ -10,7 +12,6 @@ function getCharById(req, res){
     } catch (error){
         res.status(500).json({message: error.message})
     };
-}
+};
 
-
-module.exports = getCharById;
+module.exports = {getCharById};
